5ec3966b5805bf3a5b7c9c197491b7017c3930c9
Wo-ogie <siwall0105@gmail.com>
2023-02-25 04:00:59 +0900

    chore: 프로젝트 생성 및 application.yml 설정

---
+++ src/main/java/com/zelusik/eatery/EateryApplication.java
@@ -0,0 +1,13 @@
+package com.zelusik.eatery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EateryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EateryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zelusik/eatery/EateryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zelusik.eatery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EateryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
