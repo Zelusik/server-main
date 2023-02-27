91eb759ce127df242a3f15e8fa50cc5278837fb8
Wo-ogie <siwall0105@gmail.com>
2023-02-27 22:38:17 +0900

    refactor: #7 Member entity의 약관 동의 entity null 허용으로 변경

--- src/main/java/com/zelusik/eatery/app/domain/Member.java
+++ src/main/java/com/zelusik/eatery/app/domain/Member.java
@@ -21,7 +21,7 @@ public class Member extends BaseTimeEntity {
     @Column(name = "member_id", nullable = false)
     private Long id;
 
-    @JoinColumn(name = "terms_info_id", nullable = false)
+    @JoinColumn(name = "terms_info_id")
     @OneToOne(fetch = FetchType.LAZY)
     private TermsInfo termsInfo;
 
