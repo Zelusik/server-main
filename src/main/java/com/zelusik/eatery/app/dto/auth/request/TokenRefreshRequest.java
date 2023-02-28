f015e15b8fe4cf69a05a05b5d533bbdff1a3da31
Wo-ogie <siwall0105@gmail.com>
2023-02-28 18:13:57 +0900

    docs: #7 토큰 갱신 request 데이터 명세 작성

--- src/main/java/com/zelusik/eatery/app/dto/auth/request/TokenRefreshRequest.java
+++ src/main/java/com/zelusik/eatery/app/dto/auth/request/TokenRefreshRequest.java
@@ -1,5 +1,6 @@
 package com.zelusik.eatery.app.dto.auth.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
@@ -7,6 +8,7 @@ import javax.validation.constraints.NotBlank;
 @Getter
 public class TokenRefreshRequest {
 
+    @Schema(description = "기존 발급받은 refresh token", example = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZSI6IlJPTEVfVVNFUiIsImxvZ2luVHlwZSI6IktBS0FPIiwiaWF0IjoxNjc3NTc1MTgwLCJleHAiOjE2ODAxNjcxODB9.SaUn_nlZxKiWhwm2GxGCJeC3t9XU7Gl1dLdjPc_mBFo")
     @NotBlank
     private String refreshToken;
 }
