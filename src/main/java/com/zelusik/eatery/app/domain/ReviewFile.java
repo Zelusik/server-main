b27a9e484d207feda8ce40b1cd4f631975781616
Wo-ogie <siwall0105@gmail.com>
2023-03-09 23:10:36 +0900

    refactor: #10 ReviewFile에서 파일 업로드한 사용자의 ip field 제거.
    구현이 금방 되지 않을 것 같아 후순위로 미뤄둔다.

--- src/main/java/com/zelusik/eatery/app/domain/ReviewFile.java
+++ src/main/java/com/zelusik/eatery/app/domain/ReviewFile.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@SQLDelete(sql = "UPDATE place SET deleted_at = CURRENT_TIMESTAMP WHERE place_id = ?")
+@SQLDelete(sql = "UPDATE review_file SET deleted_at = CURRENT_TIMESTAMP WHERE place_id = ?")
 @Entity
 public class ReviewFile extends BaseTimeEntity {
 
@@ -24,9 +24,6 @@ public class ReviewFile extends BaseTimeEntity {
     private Review review;
 
     @Column(nullable = false)
-    private String uploaderIp;
-
-    @Column(nullable = false)
     private String originalName;
 
     @Column(nullable = false)
@@ -37,13 +34,12 @@ public class ReviewFile extends BaseTimeEntity {
 
     private LocalDateTime deletedAt;
 
-    public static ReviewFile of(Review review, String uploaderIp, String originalName, String storedName, String url) {
-        return new ReviewFile(review, uploaderIp, originalName, storedName, url);
+    public static ReviewFile of(Review review, String originalName, String storedName, String url) {
+        return new ReviewFile(review, originalName, storedName, url);
     }
 
-    private ReviewFile(Review review, String uploaderIp, String originalName, String storedName, String url) {
+    private ReviewFile(Review review, String originalName, String storedName, String url) {
         this.review = review;
-        this.uploaderIp = uploaderIp;
         this.originalName = originalName;
         this.storedName = storedName;
         this.url = url;
