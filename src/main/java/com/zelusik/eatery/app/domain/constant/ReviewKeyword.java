af3500cf6a2e9439d7400b1ee88006120ba5de9c
Wo-ogie <siwall0105@gmail.com>
2023-03-09 22:31:38 +0900

    feat: #10 리뷰 키워드 항목 추가 및 오타 수정

--- src/main/java/com/zelusik/eatery/app/domain/constant/ReviewKeyword.java
+++ src/main/java/com/zelusik/eatery/app/domain/constant/ReviewKeyword.java
@@ -9,13 +9,15 @@ public enum ReviewKeyword {
 
     FRESH("신선한 재료"),
     BEST_FLAVOR("최고의 맛"),
-    BEST_MENU_COMBINATION("완변 메뉴조합"),
+    BEST_MENU_COMBINATION("완벽 메뉴조합"),
     GOOD_PRICE("가성비 갑"),
     GOOD_FOR_BLIND_DATE("소개팅에 최고"),
     WITH_ELDERS("웃어른과"),
     PERFECT_FOR_GROUP_MEETING("단체모임에 딱"),
     CAN_ALONE("혼밥 가능"),
     WAITING("웨이팅 있음"),
+    SILENT("조용조용한"),
+    NOISY("왁자지껄한"),
     ;
 
     private final String description;
