64a5b64efde7abb79caaac46aab343d0b7ea2de1
Wo-ogie <siwall0105@gmail.com>
2023-03-13 16:11:58 +0900

    fix: #19 access token 검증 시 로그에 log trace id가 찍히지 않는 버그 수정

---
+++ src/main/java/com/zelusik/eatery/app/config/WebFilterConfig.java
@@ -0,0 +1,29 @@
+package com.zelusik.eatery.app.config;
+
+import com.zelusik.eatery.global.log.filter.LogApiInfoFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebFilterConfig {
+
+    /**
+     * <p>
+     * API 요청/응답에 대한 로그를 출력하는 filter.
+     *
+     * <p>
+     * Spring Security에서 사용하는 filter 이전에 적용시키기 위해 Filter의 순서를 -101로 설정하였음.
+     * (Spring Security filter의 기본 순서는 -100)
+     */
+    @Bean
+    public FilterRegistrationBean<Filter> logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogApiInfoFilter());
+        filterRegistrationBean.setOrder(-101);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+}
