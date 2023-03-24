2fa53a28f2056a55178d3af0ede517df8088a211
Wo-ogie <siwall0105@gmail.com>
2023-03-24 16:39:46 +0900

    feat: #45 큐레이션 응답 데이터 중 장소 응답 재정의 - 북마크 여부 제거.

--- src/main/java/com/zelusik/eatery/app/dto/curation/response/CurationElemResponse.java
+++ src/main/java/com/zelusik/eatery/app/dto/curation/response/CurationElemResponse.java
@@ -2,6 +2,7 @@ package com.zelusik.eatery.app.dto.curation.response;
 
 import com.zelusik.eatery.app.dto.curation.CurationElemDto;
 import com.zelusik.eatery.app.dto.place.response.PlaceCompactResponse;
+import com.zelusik.eatery.app.dto.place.response.PlaceCompactResponseWithoutIsMarked;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -15,19 +16,19 @@ public class CurationElemResponse {
     private Long id;
 
     @Schema(description = "장소 정보")
-    private PlaceCompactResponse place;
+    private PlaceCompactResponseWithoutIsMarked place;
 
     @Schema(description = "이미지 url", example = "https://eatery-s3-bucket...")
     private String image;
 
-    public static CurationElemResponse of(Long id, PlaceCompactResponse place, String image) {
+    public static CurationElemResponse of(Long id, PlaceCompactResponseWithoutIsMarked place, String image) {
         return new CurationElemResponse(id, place, image);
     }
 
     public static CurationElemResponse from(CurationElemDto dto) {
         return of(
                 dto.id(),
-                PlaceCompactResponse.from(dto.placeDto()),
+                PlaceCompactResponseWithoutIsMarked.from(dto.placeDto()),
                 dto.imageDto().url()
         );
     }
