3d082a9f20ac6484994d2e96e0f7c5d546027a1f
Wo-ogie <siwall0105@gmail.com>
2023-05-10 19:15:09 +0900

    feat: #166 행정주소와 그 좌표를 함께 저장할 entity class 구현

---
+++ src/main/java/com/zelusik/eatery/domain/Location.java
@@ -0,0 +1,54 @@
+package com.zelusik.eatery.domain;
+
+import com.zelusik.eatery.domain.place.Point;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(indexes = {
+        @Index(columnList = "sido"),
+        @Index(columnList = "sgg"),
+        @Index(columnList = "emdg")
+})
+@Entity
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "location_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String sido;
+
+    private String sgg;
+
+    private String emdg;
+
+    @Embedded
+    private Point point;
+
+    public static Location of(Long id, String sido, String sgg, String emdg, Point point) {
+        return Location.builder()
+                .id(id)
+                .sido(sido)
+                .sgg(sgg)
+                .emdg(emdg)
+                .point(point)
+                .build();
+    }
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Location(Long id, String sido, String sgg, String emdg, Point point) {
+        this.id = id;
+        this.sido = sido;
+        this.sgg = sgg;
+        this.emdg = emdg;
+        this.point = point;
+    }
+}
