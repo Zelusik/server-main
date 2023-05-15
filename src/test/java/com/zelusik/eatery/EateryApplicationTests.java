572a39758787e5bc0d3bc5113c258925b5645bb6
Wo-ogie <siwall0105@gmail.com>
2023-05-15 17:14:04 +0900

    test

--- src/test/java/com/zelusik/eatery/EateryApplicationTests.java
+++ src/test/java/com/zelusik/eatery/EateryApplicationTests.java
@@ -2,9 +2,7 @@ package com.zelusik.eatery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest
 class EateryApplicationTests {
 
