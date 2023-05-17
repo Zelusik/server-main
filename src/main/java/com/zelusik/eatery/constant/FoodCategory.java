9a66318b8629e1a7da1fb012f2b768083edde093
Wo-ogie <siwall0105@gmail.com>
2023-05-17 18:45:48 +0900

    docs: #174 `FoodCategory` 예시 작성

--- src/main/java/com/zelusik/eatery/constant/FoodCategory.java
+++ src/main/java/com/zelusik/eatery/constant/FoodCategory.java
@@ -7,26 +7,25 @@ import lombok.Getter;
 
 import java.util.Arrays;
 
-@Schema(
-        description = "<p>음식 카테고리 대분류." +
-                "<p>목록은 다음과 같다." +
-                "<ul>" +
-                "<li>KOREAN - 한식" +
-                "<li>JAPANESE - 일식" +
-                "<li>CHINESE - 중식" +
-                "<li>WESTERN - 양식" +
-                "<li>MEET - 고기/구이" +
-                "<li>CHICKEN - 치킨" +
-                "<li>STREET - 분식" +
-                "<li>FAST_FOOD - 패스트푸드" +
-                "<li>DESERT - 디저트" +
-                "<li>ASIAN - 아시안푸드" +
-                "<li>SANDWICH - 샌드위치" +
-                "<li>FUSION_WORLD - 퓨전/세계" +
-                "<li>BUFFET - 뷔페" +
-                "<li>BAR - 술집" +
-                "</ul>"
-)
+@Schema(description = "<p>음식 카테고리 대분류." +
+        "<p>목록은 다음과 같다." +
+        "<ul>" +
+        "<li>KOREAN - 한식" +
+        "<li>JAPANESE - 일식" +
+        "<li>CHINESE - 중식" +
+        "<li>WESTERN - 양식" +
+        "<li>MEET - 고기/구이" +
+        "<li>CHICKEN - 치킨" +
+        "<li>STREET - 분식" +
+        "<li>FAST_FOOD - 패스트푸드" +
+        "<li>DESERT - 디저트" +
+        "<li>ASIAN - 아시안푸드" +
+        "<li>SANDWICH - 샌드위치" +
+        "<li>FUSION_WORLD - 퓨전/세계" +
+        "<li>BUFFET - 뷔페" +
+        "<li>BAR - 술집" +
+        "</ul>",
+        example = "KOREAN")
 @AllArgsConstructor
 @Getter
 public enum FoodCategory {
