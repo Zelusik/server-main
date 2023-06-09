ac5afcf3721860953596d78d00a0f6263b44b5e2
Wo-ogie <siwall0105@gmail.com>
2023-06-09 16:58:18 +0900

    chore: #196 redis cache 설정
    기본적으로는 redis cache의 serializer로 사용될 object mapper가 `LocalDateTime` type을 parsing하지 못한다. 그 때문에 jackson datatype jsr-310 dependency에 있는 `JavaTimeModule`을 object mapper에 넣어주었다.
    또한, 기본적인 상태의 `GenericJackson2JsonRedisSerializer`는 class type으로 직렬화하지 못하므로 `PolymorphicTypeValidator`를 설정해주었다.
    
    참고자료
    - [How do I enable the JSR310 support for LocalDate using Jackson?](https://stackoverflow.com/questions/69811740/how-do-i-enable-the-jsr310-support-for-localdate-using-jackson)
    - [@Cacheable java.lang.ClassCastException: java.util.LinkedHashMap cannot be cast to MyObject](https://stackoverflow.com/questions/49016372/spring-data-redis-cacheable-java-lang-classcastexception-java-util-linkedhashm)

---
+++ src/main/java/com/zelusik/eatery/config/RedisCacheConfig.java
@@ -0,0 +1,46 @@
+package com.zelusik.eatery.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@EnableCaching
+@Configuration
+public class RedisCacheConfig {
+
+    @Bean
+    public CacheManager defaultCacheManager(RedisConnectionFactory cf) {
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(cf).cacheDefaults(createRedisCacheConfig(60)).build();
+    }
+
+    private static RedisCacheConfiguration createRedisCacheConfig(long cacheTtlMin) {
+        return RedisCacheConfiguration
+                .defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(createObjMapperForRedisCacheSerializer()))) // Value Serializer 변경
+                .entryTtl(Duration.ofMinutes(cacheTtlMin));
+    }
+
+    private static ObjectMapper createObjMapperForRedisCacheSerializer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder().allowIfSubType(Object.class).build();
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL);
+        return objectMapper;
+    }
+}
