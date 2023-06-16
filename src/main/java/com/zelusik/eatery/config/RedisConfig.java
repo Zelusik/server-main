ac54e8d83808212c2fb34a1ebf6324a4e26c6b19
Wo-ogie <siwall0105@gmail.com>
2023-06-17 01:14:16 +0900

    chore: #202 용도에 따라 redis database 분리하여 데이터를 저장하도록 설정 추가

---
+++ src/main/java/com/zelusik/eatery/config/RedisConfig.java
@@ -0,0 +1,45 @@
+package com.zelusik.eatery.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    private final String host;
+    private final int port;
+
+    public RedisConfig(
+            @Value("${spring.redis.host}") String host,
+            @Value("${spring.redis.port}") int port
+    ) {
+        this.host = host;
+        this.port = port;
+    }
+
+    @Primary
+    @Bean
+    public RedisConnectionFactory defaultRedisConnectionFactory() {
+        return createLettuceConnectionFactory(0);
+    }
+
+    @Qualifier("redisConnectionFactoryForCaching")
+    @Bean
+    public RedisConnectionFactory redisConnectionFactoryForCaching() {
+        return createLettuceConnectionFactory(1);
+    }
+
+    private RedisConnectionFactory createLettuceConnectionFactory(int dbIndex) {
+        RedisStandaloneConfiguration redisStandaloneConfig = new RedisStandaloneConfiguration();
+        redisStandaloneConfig.setHostName(host);
+        redisStandaloneConfig.setPort(port);
+        redisStandaloneConfig.setDatabase(dbIndex);
+        return new LettuceConnectionFactory(redisStandaloneConfig);
+    }
+}
