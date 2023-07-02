1c42064ffb03409e4e6577841357dc7f95c24359
Wo-ogie <siwall0105@gmail.com>
2023-07-02 23:49:01 +0900

    refactor: #208 `Gson` 객체를 spring bean으로 등록하는 설정 코드 제거
    현재, gson을 사용하고 있지 않기도 하고, 
    추후 필요할 때 gson builder를 통해 커스텀한 객체를 등록하는 것이 좋을 것 같다고 생각하였다.
    현재 불필요하므로 우선 제거해둠.

--- src/main/java/com/zelusik/eatery/config/EateryBeanFactory.java
+++ src/main/java/com/zelusik/eatery/config/EateryBeanFactory.java
@@ -1,17 +1,11 @@
 package com.zelusik.eatery.config;
 
-import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class EateryBeanFactory {
-
-    @Bean
-    public Gson gson() {
-        return new Gson();
-    }
 
     @Bean
     public RestTemplate restTemplate() {
