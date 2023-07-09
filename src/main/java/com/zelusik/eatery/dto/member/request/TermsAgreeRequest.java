d6a962a2184847ea4cdd447f40d5cda4e692b7d3
Wo-ogie <siwall0105@gmail.com>
2023-07-10 05:48:57 +0900

    refactor: 약관 동의 API validation 설정 추가

--- src/main/java/com/zelusik/eatery/dto/member/request/TermsAgreeRequest.java
+++ src/main/java/com/zelusik/eatery/dto/member/request/TermsAgreeRequest.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -15,20 +16,25 @@ public class TermsAgreeRequest {
 
     @Schema(description = "미성년자 여부. 만 14세 이상이라면 true", example = "true")
     @AssertTrue
+    @NotNull
     private Boolean isNotMinor;
 
     @Schema(description = "서비스 이용약관 동의", example = "true")
     @AssertTrue
+    @NotNull
     private Boolean service;
 
     @Schema(description = "개인정보 수집/이용 동의", example = "true")
     @AssertTrue
+    @NotNull
     private Boolean userInfo;
 
     @Schema(description = "위치 정보 제공 동의", example = "true")
+    @NotNull
     private Boolean locationInfo;
 
     @Schema(description = "마케팅 수신 동의", example = "false")
+    @NotNull
     private Boolean marketingReception;
 
     public static TermsAgreeRequest of(Boolean isNotMinor, Boolean service, Boolean userInfo, Boolean locationInfo, Boolean marketingReception) {
