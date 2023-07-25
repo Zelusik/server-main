e86c257e8de0b8b161d798a9dbfd7cc3de11bec1
Wo-ogie <siwall0105@gmail.com>
2023-07-25 21:38:29 +0900

    refactor: 북마크 조회 시 연관된 entity에 대해 fetch join 적용

--- src/main/java/com/zelusik/eatery/repository/bookmark/BookmarkRepository.java
+++ src/main/java/com/zelusik/eatery/repository/bookmark/BookmarkRepository.java
@@ -1,11 +1,10 @@
 package com.zelusik.eatery.repository.bookmark;
 
 import com.zelusik.eatery.domain.Bookmark;
-import com.zelusik.eatery.domain.member.Member;
 import com.zelusik.eatery.domain.place.Place;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Map;
 import java.util.Optional;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
@@ -14,5 +13,6 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     boolean existsByMember_IdAndPlace_Id(Long memberId, Long placeId);
 
+    @EntityGraph(attributePaths = {"member", "place"})
     Optional<Bookmark> findByMember_IdAndPlace_Id(Long memberId, Long placeId);
 }
