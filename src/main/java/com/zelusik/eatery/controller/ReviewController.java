4b4bdcdbbe51fed3937aeecfef5ac7c39583fbfe
Wo-ogie <siwall0105@gmail.com>
2023-07-31 18:19:50 +0900

    docs: 리뷰 생성 API에 대한 설명 및 에러 응답 내용 수정

--- src/main/java/com/zelusik/eatery/controller/ReviewController.java
+++ src/main/java/com/zelusik/eatery/controller/ReviewController.java
@@ -46,8 +46,7 @@ public class ReviewController {
     @ApiResponses({
             @ApiResponse(description = "Created", responseCode = "201", content = @Content(schema = @Schema(implementation = ReviewResponse.class))),
             @ApiResponse(description = "[1001] 전달받은 파일을 읽을 수 없는 경우.", responseCode = "400", content = @Content),
-            @ApiResponse(description = "[1350] 장소에 대한 추가 정보를 스크래핑 할 Flask 서버에서 에러가 발생한 경우.", responseCode = "500", content = @Content),
-            @ApiResponse(description = "[3000] 상세 페이지에서 읽어온 가게 영업시간이 처리할 수 없는 형태일 경우.", responseCode = "500", content = @Content)
+            @ApiResponse(description = "[1350] 장소에 대한 추가 정보를 스크래핑 할 Scraping server에서 에러가 발생한 경우.", responseCode = "500", content = @Content),
     })
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ReviewResponse> create(
