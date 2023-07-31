534646c7191c4b3f0c505b4ab527f1eb52c76aee
Wo-ogie <siwall0105@gmail.com>
2023-07-31 16:47:28 +0900

    refactor: 더는 사용하지 않는 불필요한 예외(`OpeningHoursUnexpectedFormatException`) 삭제
    해당 예외는 Scraping server에서 예외처리를 보완함에 따라 더 이상 필요하지 않아졌다. 이에 삭제함.
    
    관련 예외
    - #189

--- src/main/java/com/zelusik/eatery/exception/scraping/OpeningHoursUnexpectedFormatException.java
+++
@@ -1,6 +0,0 @@
-package com.zelusik.eatery.exception.scraping;
-
-import com.zelusik.eatery.exception.common.InternalServerException;
-
-public class OpeningHoursUnexpectedFormatException extends InternalServerException {
-}
