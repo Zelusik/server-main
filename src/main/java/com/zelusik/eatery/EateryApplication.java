cedc33d507d23d1bd2a6e69045fea97c9f2a91f0
Wo-ogie <siwall0105@gmail.com>
2023-08-28 18:54:53 +0900

    refactor: #325 `@PropertySource`를 통해 환경 변수를 주입받도록 수정

--- src/main/java/com/zelusik/eatery/EateryApplication.java
+++ src/main/java/com/zelusik/eatery/EateryApplication.java
@@ -2,7 +2,9 @@ package com.zelusik.eatery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:/env.properties")
 @SpringBootApplication
 public class EateryApplication {
 
