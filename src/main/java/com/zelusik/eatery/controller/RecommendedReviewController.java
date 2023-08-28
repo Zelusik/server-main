f34029ff68ab07c94defe2f5b25e6f694fcb3dfd
Wo-ogie <siwall0105@gmail.com>
2023-08-29 04:39:02 +0900

    feat: #335 추천 리뷰 수정 API 구현

--- src/main/java/com/zelusik/eatery/controller/RecommendedReviewController.java
+++ src/main/java/com/zelusik/eatery/controller/RecommendedReviewController.java
@@ -1,7 +1,9 @@
 package com.zelusik.eatery.controller;
 
 import com.zelusik.eatery.dto.recommended_review.RecommendedReviewDto;
+import com.zelusik.eatery.dto.recommended_review.request.BatchUpdateRecommendedReviewsRequest;
 import com.zelusik.eatery.dto.recommended_review.request.SaveRecommendedReviewsRequest;
+import com.zelusik.eatery.dto.recommended_review.response.BatchUpdateRecommendedReviewsResponse;
 import com.zelusik.eatery.dto.recommended_review.response.SaveRecommendedReviewsResponse;
 import com.zelusik.eatery.security.UserPrincipal;
 import com.zelusik.eatery.service.RecommendedReviewService;
@@ -11,13 +13,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 
 @Tag(name = "추천 리뷰 관련 API")
 @RequiredArgsConstructor
@@ -41,5 +41,20 @@ public class RecommendedReviewController {
         return ResponseEntity
                 .created(URI.create("/api/members/recommended-reviews/" + recommendedReviewDto.getId()))
                 .body(SaveRecommendedReviewsResponse.from(recommendedReviewDto));
+    }
+
+    @Operation(
+            summary = "추천 리뷰 목록 갱신",
+            description = "<p>추천 리뷰를 갱신한다.\n" +
+                          "<p>기존 등록된 추천 리뷰 내역을 전부 삭제한 후, 새로 전달받은 추천 리뷰 목록으로 대체한다.",
+            security = @SecurityRequirement(name = "access-token")
+    )
+    @PutMapping("/batch-update")
+    public BatchUpdateRecommendedReviewsResponse batchUpdateRecommendedReviews(
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @RequestBody @Valid BatchUpdateRecommendedReviewsRequest saveRecommendedReviewsRequest
+    ) {
+        List<RecommendedReviewDto> recommendedReviewDtos = recommendedReviewService.batchUpdateRecommendedReviews(userPrincipal.getMemberId(), saveRecommendedReviewsRequest);
+        return BatchUpdateRecommendedReviewsResponse.from(recommendedReviewDtos);
     }
 }
