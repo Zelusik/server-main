5bf3f102d2ae467c85a76e2dd2bdf732e4397507
Wo-ogie <siwall0105@gmail.com>
2023-08-28 19:51:34 +0900

    feat: #330 추천 리뷰 entity 구현

---
+++ src/main/java/com/zelusik/eatery/domain/RecommendedReview.java
@@ -0,0 +1,28 @@
+package com.zelusik.eatery.domain;
+
+import com.zelusik.eatery.domain.member.Member;
+import com.zelusik.eatery.domain.review.Review;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class RecommendedReview extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recommended_review_id")
+    private Long id;
+
+    @JoinColumn(name = "member_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @JoinColumn(name = "review_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Review review;
+}
