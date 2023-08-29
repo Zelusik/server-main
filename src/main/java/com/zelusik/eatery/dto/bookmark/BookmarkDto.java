877d401b360883210f0194dc54c3fb4bcc579544
Wo-ogie <siwall0105@gmail.com>
2023-08-30 06:12:13 +0900

    refactor: #354 북마크 dto 불필요한 필드 삭제

--- src/main/java/com/zelusik/eatery/dto/bookmark/BookmarkDto.java
+++ src/main/java/com/zelusik/eatery/dto/bookmark/BookmarkDto.java
@@ -14,20 +14,16 @@ public class BookmarkDto {
     private Long id;
     private Long memberId;
     private Long placeId;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 
-    public static BookmarkDto of(Long id, Long memberId, Long placeId, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        return new BookmarkDto(id, memberId, placeId, createdAt, updatedAt);
+    public static BookmarkDto of(Long id, Long memberId, Long placeId) {
+        return new BookmarkDto(id, memberId, placeId);
     }
 
     public static BookmarkDto from(Bookmark entity) {
         return of(
                 entity.getId(),
                 entity.getMember().getId(),
-                entity.getPlace().getId(),
-                entity.getCreatedAt(),
-                entity.getUpdatedAt()
+                entity.getPlace().getId()
         );
     }
 }
