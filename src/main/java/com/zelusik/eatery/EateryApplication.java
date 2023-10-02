d2ad1c14290c1b7974bd48d87b35f9406153c15b
Wo-ogie <siwall0105@gmail.com>
2023-10-02 15:41:28 +0900

    chore: #321 Spring Configuration Processor 의존성 추가 및 설정

--- src/main/java/com/zelusik/eatery/EateryApplication.java
+++ src/main/java/com/zelusik/eatery/EateryApplication.java
@@ -2,8 +2,10 @@ package com.zelusik.eatery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.PropertySource;
 
+@ConfigurationPropertiesScan
 @PropertySource("classpath:/env.properties")
 @SpringBootApplication
 public class EateryApplication {
@@ -11,5 +13,4 @@ public class EateryApplication {
     public static void main(String[] args) {
         SpringApplication.run(EateryApplication.class, args);
     }
-
 }
