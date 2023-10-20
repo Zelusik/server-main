7f16eb7d7c53c9582af92a1df7ca503fd244bbc6
Wo-ogie <siwall0105@gmail.com>
2023-10-20 22:17:18 +0900

    fix: logging pointcouts 경로가 잘못 설정되어 있어 로그 기록이 남지 않는 버그 수정

--- src/main/java/com/zelusik/eatery/global/log/aop/Pointcuts.java
+++ src/main/java/com/zelusik/eatery/global/log/aop/Pointcuts.java
@@ -4,12 +4,12 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class Pointcuts {
 
-    @Pointcut("execution(* com.zelusik.eatery.controller..*(..))")
+    @Pointcut("execution(* com.zelusik.eatery.*..controller..*(..))")
     public void controllerPoint(){}
 
-    @Pointcut("execution(* com.zelusik.eatery.service..*(..))")
+    @Pointcut("execution(* com.zelusik.eatery.*..service..*(..))")
     public void servicePoint(){}
 
-    @Pointcut("execution(* com.zelusik.eatery.repository..*(..))")
+    @Pointcut("execution(* com.zelusik.eatery.*..repository..*(..))")
     public void repositoryPoint(){}
 }
