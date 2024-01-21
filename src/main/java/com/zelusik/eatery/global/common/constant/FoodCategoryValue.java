29be65ae9c5616daf167318b1da51c6d742af625
Wo-ogie <siwall0105@gmail.com>
2024-01-21 19:54:23 +0900

    docs: #409 `FoodCategoryValue`에 대한 API 명세 내용 수정

--- src/main/java/com/zelusik/eatery/global/common/constant/FoodCategoryValue.java
+++ src/main/java/com/zelusik/eatery/global/common/constant/FoodCategoryValue.java
@@ -7,24 +7,7 @@ import lombok.Getter;
 import java.util.Arrays;
 import java.util.List;
 
-@Schema(description = "<p>음식 종류. 목록은 다음과 같다." +
-                      "<ul>" +
-                      "   <li><code>KOREAN</code> - 한식</li>" +
-                      "   <li><code>JAPANESE</code> - 일식</li>" +
-                      "   <li><code>CHINESE</code> - 중식</li>" +
-                      "   <li><code>WESTERN</code> - 양식</li>" +
-                      "   <li><code>MEAT</code> - 고기/구이</li>" +
-                      "   <li><code>CHICKEN</code> - 치킨</li>" +
-                      "   <li><code>STREET</code> - 분식</li>" +
-                      "   <li><code>FAST_FOOD</code> - 패스트푸드</li>" +
-                      "   <li><code>CAFE_DESSERT</code> - 카페/디저트</li>" +
-                      "   <li><code>ASIAN</code> - 아시안푸드</li>" +
-                      "   <li><code>SANDWICH</code> - 샌드위치</li>" +
-                      "   <li><code>FUSION_WORLD</code> - 퓨전/세계</li>" +
-                      "   <li><code>BUFFET</code> - 뷔페</li>" +
-                      "   <li><code>BAR</code> - 술집</li>" +
-                      "</ul>",
-        example = "KOREAN")
+@Schema(description = "Eatery 음식/음식점 카테고리")
 @AllArgsConstructor
 @Getter
 public enum FoodCategoryValue {
